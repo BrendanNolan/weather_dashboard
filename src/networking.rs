a5363b56bed33f382f8dff5b68fd1b64b3ff1bd4
Brendan Nolan <bnolan23@gmail.com>
2023-06-25 14:12:45 +0100

    Some Progress With Networking

--- src/networking.rs
+++ src/networking.rs
@@ -1,6 +1,6 @@
 use client::{command::Command, tasks};
 use connection_utils::ServerError;
-use std::sync::mpsc::Sender as StdSender;
+use std::sync::mpsc::{Receiver as StdReceiver, Sender as StdSender};
 use tokio::{
     net::TcpStream,
     sync::{
@@ -13,37 +13,52 @@ use crate::{app_state::County, weather_report::WeatherReport};
 
 type WeatherCommand = Command<County, (County, WeatherReport)>;
 
-pub async fn run_client(tx_results: StdSender<(County, WeatherReport)>) {
+pub async fn run_client(
+    rx_county: StdReceiver<County>,
+    tx_results: StdSender<(County, WeatherReport)>,
+) {
     let (tx, rx) = mpsc::channel::<WeatherCommand>(32);
     let stream = TcpStream::connect("127.0.0.1:6379").await.unwrap();
     let manager = tokio::spawn(tasks::create_cyclic_connection_manager(stream, rx));
-    let client = tokio::spawn(create_client_task(tx, tx_results));
+    let client = tokio::spawn(create_client_task(rx_county, tx, tx_results));
     client.await.unwrap();
     manager.await.unwrap();
 }
 
 async fn create_client_task(
+    rx_county: StdReceiver<County>,
     tx: TokioSender<WeatherCommand>,
     tx_results: StdSender<(County, WeatherReport)>,
 ) {
-    for i in 0..10 {
-        let (response_tx, response_rx) = oneshot::channel();
-        tx.send(WeatherCommand {
-            data: County(format!("County_{i}")),
-            responder: response_tx,
-        })
-        .await
-        .unwrap();
-        let response = response_rx.await;
-        match response {
-            Ok(Some(response)) => match process_response_from_server(&response) {
-                ClientAction::Continue => {}
-                ClientAction::Stop => return,
+    let mut response_rx = None;
+    loop {
+        tokio::select! {
+            Some(county) = try_recv(&rx_county) => {
+                let (response_tx, rx) = oneshot::channel();
+                response_rx = Some(rx);
+                tx.send(WeatherCommand {
+                    data: county,
+                    responder: response_tx,
+                })
+                .await
+                .unwrap();
             },
-            Ok(None) => println!("Failed to read response."),
-            Err(_) => panic!("Client unexpectedly failed to receive a response"),
+            response = response_rx => {
+                match response {
+                    Ok(Some(response)) => match process_response_from_server(&response) {
+                        ClientAction::Continue => {}
+                        ClientAction::Stop => return,
+                    },
+                    Ok(None) => println!("Failed to read response."),
+                    Err(_) => panic!("Client unexpectedly failed to receive a response"),
+                }
+            },
         }
     }
+}
+
+async fn try_recv(rx: &StdReceiver<County>) -> Option<County> {
+    rx.try_recv().ok()
 }
 
 enum ClientAction {
